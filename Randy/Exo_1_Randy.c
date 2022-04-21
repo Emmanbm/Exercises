b5ab9bb5254477ea415ae6d4c0e83a68fd62edde
Emmanbm <93186313+Emmanbm@users.noreply.github.com>
2022-04-21 14:37:47 +0200

    Create Exo_1_Randy.c

---
+++ Randy/Exo_1_Randy.c
@@ -0,0 +1,12 @@
+//Crée un programme en langage C qui calcule et affiche le périmètre d'un rectange de longueur = 7.5 et de largeur = 2.4
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /*
+  Complète ton code ici, juste après ce commentaire
+  */
+  
+  return 0;
+}
