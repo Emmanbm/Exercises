1d95c3c503cced5183ea578cce4e4c01a9bf7c91
Emmanbm <93186313+Emmanbm@users.noreply.github.com>
2022-04-21 14:43:45 +0200

    Create exo_1_Ledoux.c

---
+++ Ledoux/exo_1_Ledoux.c
@@ -0,0 +1,3 @@
+//Crée une fonction qui vas nommer max et qui va retourner quel nombre est supérieur entre deux nombres passés en paramètres.
+
+//Exemple max(6 , 8) doit te retourner "8 est supérieur à 6"
