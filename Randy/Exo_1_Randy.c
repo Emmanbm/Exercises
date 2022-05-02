aaa381cdcca19662534d26792769f7a934a78532
Randy243 <104146055+Randy243@users.noreply.github.com>
2022-05-02 20:29:10 +0100

    Create Exo_1_Randy.c

--- Randy/Exo_1_Randy.c
+++ Randy/Exo_1_Randy.c
@@ -4,9 +4,11 @@
 
 int main()
 {
-  /*
-  Complète ton code ici, juste après ce commentaire
-  */
+  //Crée un programme en langage C qui calcule et affiche le périmètre d'un rectange de longueur = 7.5 et de largeur = 2.4
+float longueur = 7.5;
+float largeur = 2.4;
+float perimetredunrectangle = 2 * (longueur + largeur);
+printf("%f + %f * 2 = %f", longueur, largeur, perimetredunrectangle);
   
   return 0;
 }
