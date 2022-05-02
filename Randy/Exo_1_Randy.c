1db62be053a3009b30e016138d668a2179178b0f
Emmanbm <93186313+Emmanbm@users.noreply.github.com>
2022-05-02 21:48:33 +0200

    Update Exo_1_Randy.c

--- Randy/Exo_1_Randy.c
+++ Randy/Exo_1_Randy.c
@@ -12,3 +12,13 @@ printf("%f + %f * 2 = %f", longueur, largeur, perimetredunrectangle);
   
   return 0;
 }
+
+//Très bien. La réponse est bonne mais il y a certains points à améliorer pour un meilleur code
+
+/*
+  - Quand tu déclares deux variables de même type tu peux les déclarer sur la même ligne
+      - float longueur = 7.5, largeur = 2.4;
+  - Je te conseille de toujours bien indenter tes instructions; ce n'est pas obligatoire mais cela te permet à toi et aux gens qui liront ton code de mieux le lire
+  - Ça aussi ce n'est pas obligatoire, mais dans printf tu aurais dû mettre *2 en évidence pour respecter les règles de priorité
+    printf("(%f + %f) * 2 = %f", longueur, largeur, perimetredunrectangle);
+*/
